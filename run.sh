d92876d1abd6054703f15658132a8bb4957aa1ae
Dana Merrick <dana.merrick@gmail.com>
2020-06-02 12:07:34 -0400

    Updates

--- run.sh
+++ run.sh
@@ -5,20 +5,19 @@ set -ex
 NAME="obs-on-docker"
 
 # sudo apt install nvidia-container-runtime
-docker run -it --rm \
-  --runtime=nvidia \
-  --privileged \
-  -p 5900:5900 \
-  -p 5901:5901 \
+# docker run -it --rm \
+#   --runtime=nvidia \
+#   --privileged \
+#   -p 5900:5900 \
+#   $NAME
 
 # bind container port 5900 to local 5902 (vnc)
-# bind container port 5901 to local 5980 (http)
-# docker run -it --rm \
-#   --shm-size=256m \
-#   -e VNC_PASSWD=123456 \
-#   -p 5902:5900 \
-#   -p 5980:5901 \
-#   $NAME
+docker run -it --rm \
+  --shm-size=256m \
+  -e VNC_PASSWD=123456 \
+  --privileged \
+  -p 5902:5900 \
+  $NAME
 
 #TODO: tail the obs log here?
 #  tail -f /root/.config/obs-studio/logs/*.txt
