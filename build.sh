235ae4e74f2ff7d5a6285270e086849230967f1c
Dana Merrick <dana.merrick@gmail.com>
2020-06-02 01:02:49 +0000

    Progress

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# enable buildkit
+# https://www.docker.com/blog/faster-builds-in-compose-thanks-to-buildkit-support/
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
+
 set -ex
 
 NAME="obs-on-docker"
